0ab882950fbcb68c4861d4e8068379bb7436f851
paulodelosrey <135145158+paulodelosrey@users.noreply.github.com>
2024-09-06 17:20:10 -0500

    Se corrige ruta de imagenes de nuevo

--- src/components/Phone.vue
+++ src/components/Phone.vue
@@ -2,7 +2,8 @@
 <div class="mockup-phone">
   <div class="camera"></div>
   <div class="display">
-    <div class="artboard artboard-demo phone-1 text-center">Contactame:<br>paulodelosrey@gmail.com</div>
+    <div class="artboard artboard-demo phone-1 text-center"><strong>Contáctame para mas información:</strong><br>Correo electrónico:<br>paulodelosrey@gmail.com<br><br>O encuéntrame en:<br><a href="https://www.linkedin.com/in/paulodelosreyes/" class="text-blue-500 font-bold">Linkedin</a>
+    </div>
   </div>
 </div>
 </template>
